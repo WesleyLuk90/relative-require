614f6fea64cea90707dc2bd34461b39d6560070c
Wesley Luk <wesleyluk@gmail.com>
2017-02-18 16:43:13 -0800

    Fixed tests and support export parsing

--- spec/analyse-source-spec.js
+++ spec/analyse-source-spec.js
@@ -14,6 +14,12 @@ describe('analyseSource function', () => {
       pos: 21
     })
   })
+  it('should return the correct type for a source with only import statements and jsx', () => {
+    expect(analyseSource('import foo from \'foo\'; const value = (<div />);')).toEqual({
+      type: 'import',
+      pos: 22
+    })
+  })
 
   it('should return type `require` for a source with the same number of require & import statements', () => {
     expect(analyseSource('const foo = require(\'foo\')\nimport foo from \'foo\'')).toEqual({
@@ -39,6 +45,25 @@ describe('analyseSource function', () => {
   it('should return the default values when the active file source cannot be parsed', () => {
     expect(analyseSource('some invalid source')).toEqual({
       type: 'require',
+      pos: 0
+    })
+  })
+
+  it('should return import values when the active file has an export default statement', () => {
+    expect(analyseSource('export default class MyClass {}')).toEqual({
+      type: 'import',
+      pos: 0
+    })
+  })
+  it('should return import values when the active file has an export statement', () => {
+    expect(analyseSource('export {value}')).toEqual({
+      type: 'import',
+      pos: 0
+    })
+  })
+  it('should return import values when the active file has an export variable', () => {
+    expect(analyseSource('export const val = 10')).toEqual({
+      type: 'import',
       pos: 0
     })
   })
--- spec/statement-helpers-spec.js
+++ spec/statement-helpers-spec.js
@@ -9,29 +9,29 @@ describe('pathToStatement function', () => {
 
   describe('with type `require`', () => {
     it('should create the expected relative require statement', () => {
-      expect(convert('require', '/foo')('/foo/bar.js')).toBe('const bar = require(\'./bar\')')
-      expect(convert('require', '/foo/bar')('/foo/bar.js')).toBe('const bar = require(\'../bar\')')
-      expect(convert('require', '/foo/bar')('/foo/baz/bar.js')).toBe('const bar = require(\'../baz/bar\')')
+      expect(convert('require', '/foo')('/foo/bar.js')).toContain('const bar = require(\'./bar\')')
+      expect(convert('require', '/foo/bar')('/foo/bar.js')).toContain('const bar = require(\'../bar\')')
+      expect(convert('require', '/foo/bar')('/foo/baz/bar.js')).toContain('const bar = require(\'../baz/bar\')')
     })
     it('should camelCase the variable name', () => {
-      expect(convert('require', '/foo')('/foo/bar-baz.js')).toBe('const barBaz = require(\'./bar-baz\')')
+      expect(convert('require', '/foo')('/foo/bar-baz.js')).toContain('const barBaz = require(\'./bar-baz\')')
     })
   })
 
   describe('with type `import`', () => {
     it('should create the expected relative import statement', () => {
-      expect(convert('import', '/foo')('/foo/bar.js')).toBe('import bar from \'./bar\'')
-      expect(convert('import', '/foo/bar')('/foo/bar.js')).toBe('import bar from \'../bar\'')
-      expect(convert('import', '/foo/bar')('/foo/baz/bar.js')).toBe('import bar from \'../baz/bar\'')
+      expect(convert('import', '/foo')('/foo/bar.js')).toContain('import bar from \'./bar\'')
+      expect(convert('import', '/foo/bar')('/foo/bar.js')).toContain('import bar from \'../bar\'')
+      expect(convert('import', '/foo/bar')('/foo/baz/bar.js')).toContain('import bar from \'../baz/bar\'')
     })
     it('should camelCase the variable name', () => {
-      expect(convert('import', '/foo')('/foo/bar-baz.js')).toBe('import barBaz from \'./bar-baz\'')
+      expect(convert('import', '/foo')('/foo/bar-baz.js')).toContain('import barBaz from \'./bar-baz\'')
     })
   })
 
   it('should create the correct import statements for index files', () => {
-    expect(convert('import', '/foo')('/bar/index.js')).toBe('import bar from \'../bar\'')
-    expect(convert('import', '/foo')('/bar/index.html')).toBe('import index from \'../bar/index.html\'')
+    expect(convert('import', '/foo')('/bar/index.js')).toContain('import bar from \'../bar\'')
+    expect(convert('import', '/foo')('/bar/index.html')).toContain('import index from \'../bar/index.html\'')
   })
 })
 
@@ -43,10 +43,10 @@ describe('pathsToStatements function', () => {
   })
 
   it('should dedupe modules with the same path after removal of extension', () => {
-    expect(convert('require', '/foo', [ '/foo/bar.js', '/foo/bar.jsx', '/foo/bar.json', '/foo/baz.js' ])).toEqual([
-      'const bar = require(\'./bar\')',
-      'const bar = require(\'./bar.json\')',
-      'const baz = require(\'./baz\')'
+    expect(convert('require', '/foo', ['/foo/bar.js', '/foo/bar.jsx', '/foo/bar.json', '/foo/baz.js'])).toEqual([
+      'const bar = require(\'./bar\');',
+      'const bar = require(\'./bar.json\');',
+      'const baz = require(\'./baz\');'
     ])
   })
 })
